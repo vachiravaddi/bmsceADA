efaab4039664985e957a5015309f80aa02e261b2
vachiravaddi <109196614+vachiravaddi@users.noreply.github.com>
2022-07-26 10:34:34 +0530

    Create quickSort_2.c

---
+++ quickSort_2.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#include<time.h>
+#include<math.h>
+#include<stdlib.h>
+void quicksort(int number[5000],int first,int last)
+    {
+       int i, j, pivot, temp;
+       if(first<last)
+    {
+          pivot=first;
+    i=first;
+          j=last;
+          while(i<j)
+    {
+    for(int x=0;x<10000000;x++);
+            while(number[i]<=number[pivot]&&i<last)
+                i++;
+             while(number[j]>number[pivot])
+             j--;
+             if(i<j)
+    {
+                temp=number[i];
+                number[i]=number[j];
+                number[j]=temp;
+             }
+          }
+          temp=number[pivot];
+          number[pivot]=number[j];
+          number[j]=temp;
+          quicksort(number,first,j-1);
+          quicksort(number,j+1,last);
+       }
+    }
+    int main()
+    {
+    clock_t start,end;
+       int i, count, number[5000];
+    printf("No. of elements: ");
+    scanf("%d",&count);
+    printf("Enter %d elements: ", count);
+       for(i=0;i<count;i++)
+    {
+           number[i]=rand()%5000;
+       }
+       start = clock();
+       quicksort(number,0,count-1);
+       end = clock();
+    printf("Order of Sorted elements: ");
+       for(i=0;i<count;i++)
+    {
+    printf(" %d",number[i]);
+    }
+    printf("\nSeconds taken %lf",(double)(end-start)/CLOCKS_PER_SEC);
+       return 0;
+    }
